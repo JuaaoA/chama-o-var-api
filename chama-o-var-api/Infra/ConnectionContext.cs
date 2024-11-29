96970e649edd906cf394e700c56d97473ebb9f52
Juaocareca <joaoaugusto-santana@hotmail.com>
2024-11-28 21:55:29 -0300

    nem sei oq mudei

--- chama-o-var-api/Infra/ConnectionContext.cs
+++ chama-o-var-api/Infra/ConnectionContext.cs
@@ -15,8 +15,6 @@ namespace chama_o_var_api.Infra
         public DbSet<Evento> Eventos { get; set; }
         public DbSet<Ingresso> Ingressos { get; set; }
 
-        // TODO - CRIAR CONFIGS
-
         // BANCO DE DADOS
         string mysqlConnection = "Server=localhost;" +
             "Port=3306;" +
